697f8c55ae0423bf1b18b0ad86b9680155c6bc65
erikcampbell <86338141+erikcampbell@users.noreply.github.com>
2023-05-02 19:12:09 -0700

    # Initial Commit

---
+++ code/working_hours_dtc.R
@@ -0,0 +1,41 @@
+# OMSBA 5305 Spring 2023
+# Data Translation Challenge
+# May, Jarrod
+# Vierela, Michael
+# Hales, Chris
+
+library(tidyverse)
+library(stats)
+library(forecast)
+library(readxl)
+
+# load data and convert to time-series
+data <- read_excel("./Data/ch10_Weekly_Hours.xls")
+
+
+# plot and review for trends/seasonality
+
+# first difference, log and/or first difference and log?
+
+#plot stationarity
+
+# What is ACF and PACF?
+
+# Choose 3 linear models (i.e. AR, MA, ARMA) and estimate
+
+# present:
+
+# Estimation Results
+
+# ACF Correlogram
+
+# PACF Correlogram
+
+#Q-Test
+
+# Summarize all model estimation + evaluation into table (emulate table 8.2)
+
+
+# Make 6 month/period forecast
+
+# plot multistep of forecast
