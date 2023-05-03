69c9bf94d9d6113953e17e6dca4be7fc7d51df49
erikcampbell <86338141+erikcampbell@users.noreply.github.com>
2023-05-02 19:45:37 -0700

    # EDA plot for data

--- code/working_hours_dtc.R
+++ code/working_hours_dtc.R
@@ -8,12 +8,19 @@ library(tidyverse)
 library(stats)
 library(forecast)
 library(readxl)
+library(lubridate)
 
 # load data and convert to time-series
 data <- read_excel("./Data/ch10_Weekly_Hours.xls")
 
 
 # plot and review for trends/seasonality
+plot(data, main = "Average Weekly Hours All Employees from MAR 2006 - MAR 2023",
+     xlab = 'Month/Year', ylab = 'Mean Weekly Hours indexed to 2007= 100')
+
+# turn into time-series
+
+# plot
 
 # first difference, log and/or first difference and log?
 
